d03a14a060203ee7ce3fec231b0b0f716bb960a3
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2023-12-09 22:47:02 +0200

    feat: add smooth scrolling to footer-menu page

--- pages/menu/script.js
+++ pages/menu/script.js
@@ -26,3 +26,14 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+
+// Add smooth scrolling behavior to anchor links
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth',
+        });
+    });
+});
